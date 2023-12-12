34c2b71b388f1bee30e2087ac72f94826bb3aad3
Keon <Keon.ke@everfortune.ai>
2023-12-12 13:49:25 +0800

    ✨ feat: add IndexedDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,70 @@
 import { useState } from 'react'
 import './App.css'
-import { initDB } from './db'
+import { Stores, addData, initDB } from './db'
 
 function App() {
   const [isDBReady, setIsDBReady] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const handleAdd = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const target = e.target as typeof e.target & {
+      name:{ value:string }
+      email:{ value:string }
+    }
+    const name = target.name.value
+    const email = target.email.value
+    const id = Date.now()
+
+    if (name.trim() === '' || email.trim() === '') {
+      alert('please enter the value')
+      return
+    }
+
+    try {
+      await addData(Stores.Users, { name, email, id })
+    } catch (err:unknown) {
+      if (err instanceof Error) {
+        setErrorMsg(err.message)
+      } else {
+        setErrorMsg('Unknown Error')
+      }
+    }
+  }
 
   return (
     <>
       <h1>Index DataBase</h1>
-      {isDBReady ? <h2>DB is ready</h2> : (
+      {!isDBReady ? (
         <button
           type="button"
           onClick={async () => {
-            const status = await initDB()
+            await initDB()
             setIsDBReady(true)
           }}
         >Init DB
         </button>
+      ) : (
+        <>
+          <h2>DB is ready</h2>
+          <form onSubmit={handleAdd}>
+            <input type="text" name="name" id="Name" placeholder="Enter Name" />
+            <input type="email" name="email" id="Email" placeholder="Enter Email" />
+            <button type="submit">Submit</button>
+          </form>
+          {errorMsg && <p style={{ color: 'red' }}>{errorMsg}</p>}
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody />
+          </table>
+        </>
       )}
     </>
   )
--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,7 @@ let version = 1
 export interface ThisIsUser {
   id: string,
   name: string
+  email: string
 }
 
 export enum Stores {
@@ -14,7 +15,8 @@ export enum Stores {
 export const initDB = (): Promise<boolean> => {
   return new Promise((resolve) => {
     request = indexedDB.open('newDB')
-    request.onupgradeneeded = () => {
+
+    request.onupgradeneeded = (res) => {
       db = request.result
       if (!db.objectStoreNames.contains(Stores.Users)) {
         console.log('create users store')
@@ -24,8 +26,7 @@ export const initDB = (): Promise<boolean> => {
       request.onsuccess = () => {
         db = request.result
         version = db.version
-        alert(`request.onsuccess - initDB : ${version}`)
-        console.log('request.onsuccess - initDB', version)
+        alert('request.onsuccess - initDB ')
         resolve(true)
       }
 
@@ -36,3 +37,27 @@ export const initDB = (): Promise<boolean> => {
     }
   })
 }
+
+export const addData = <T>(stroeName: string, data: T): Promise<T | string> => new Promise(
+  (resolve) => {
+    request = indexedDB.open('newDB', version)
+
+    request.onsuccess = () => {
+      console.log('onsuccess - addData:', db)
+      db = request.result
+      const tx = db.transaction(stroeName, 'readwrite')
+      const store = tx.objectStore(stroeName)
+      store.add(data)
+      resolve(data)
+    }
+
+    request.onerror = () => {
+      const error = request.error?.message
+      if (error) {
+        resolve(error)
+      } else {
+        resolve('unknown error')
+      }
+    }
+  },
+)
